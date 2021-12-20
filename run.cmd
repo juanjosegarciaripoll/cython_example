0be32fc21060ce2f307c54e6ae3691d826a11e8d
Juan Jose Garcia-Ripoll <juanjose.garciaripoll@gmail.com>
2021-12-20 18:01:29 +0100

    Clean up the run.cmd script

--- run.cmd
+++ run.cmd
@@ -1,3 +1,9 @@
+@echo off
+call clean.cmd
+rem
+rem Uncomment the version (build / setuptools) you wish to use
+rem 
 python -m build
+rem python setup.py bdist_wheel
 for %%i in (dist\cython_example*.whl) do pip install --force %%i
 python test/test.py
