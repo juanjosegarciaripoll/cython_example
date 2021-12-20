dd51f8ef99b98cc87eff0553843de56c7c64324d
Juan Jose Garcia-Ripoll <juanjose.garciaripoll@gmail.com>
2021-12-20 11:46:25 +0100

    Changed the structure to cope with missing __init__.py file (Cython cannot compile it)

--- setup.py
+++ setup.py
@@ -6,11 +6,12 @@ from Cython.Build import cythonize
 setup(
     name='cython_example',
     version='0.0.1',
-    packages=find_packages(),
+    packages=['cython_example'],
     author='Juan Jose Garcia-Ripoll',
+    author_email='juanjose.garciaripoll@gmail.com',
     description="An example for a Cython-based package.",
     url='https://juanjose.garciaripoll.com',
-    ext_modules=cythonize(["cython_example/__init__.pyx"]),
+    ext_modules=cythonize(["cython_example/*.pyx"]),
     include_dirs=np.get_include(),
     install_requires=[
         'numpy',
