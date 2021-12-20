46b7d46c6f9fc56bc0dd3aab6b8e66d74ff4e331
Juan Jose Garcia-Ripoll <juanjose.garciaripoll@gmail.com>
2021-12-20 17:49:22 +0100

    Add some further compiler options for reduced code size.

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import find_packages, setup, Extension
 
 import numpy as np
 from Cython.Build import cythonize
+import Cython.Compiler.Options
 
 extensions = [
     # A single module that is stand alone and has no special requisites
@@ -13,6 +14,18 @@ extensions = [
         include_dirs=[np.get_include()])
     ]
 
+# See https://cython.readthedocs.io/en/latest/src/userguide/source_files_and_compilation.html
+# for a deeper explanation of the choices here
+Cython.Compiler.Options.docstring = False
+Cython.Compiler.Options.error_on_uninitialized = True
+directives = {
+    'language_level': '3', # We assume Python 3 code
+    'boundscheck': False, # Do not check array access
+    'wraparound': False, # a[-1] does not work
+    'embedsignature': False, # Do not save typing / docstring
+    'always_allow_keywords': False, # Faster calling conventions
+}
+
 setup(
     name='cython_example',
     version='0.0.1',
@@ -22,7 +35,7 @@ setup(
     description="An example for a Cython-based package.",
     url='https://juanjose.garciaripoll.com',
     ext_modules=cythonize(extensions, 
-                          compiler_directives={'language_level' : "3"}),
+                          compiler_directives=directives),
     include_dirs=np.get_include(),
     install_requires=[
         'numpy',
