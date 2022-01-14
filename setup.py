c07219d9b7fcbd8313798c7262e46c77869b92bd
Juan Jose Garcia-Ripoll <juanjose.garciaripoll@gmail.com>
2022-01-14 10:33:39 +0100

    Format with Black

--- setup.py
+++ setup.py
@@ -6,44 +6,47 @@ import Cython.Compiler.Options
 
 extensions = [
     # A single module that is stand alone and has no special requisites
-    Extension('cython_example.hello', ['cython_example/hello.pyx']),
-
+    Extension("cython_example.hello", ["cython_example/hello.pyx"]),
     # A module that is built from various functions, and which itself depends
     # on the numpy library (the C core of this library)
-    Extension('cython_example.matrix', ['cython_example/matrix/matrix.pyx'],
-        include_dirs=[np.get_include()]),
-
+    Extension(
+        "cython_example.matrix",
+        ["cython_example/matrix/matrix.pyx"],
+        include_dirs=[np.get_include()],
+    ),
     # A module that is built with Cython and which demonstrates C classes
     # and headers to use the C functions from other modules
-    Extension('cython_example.random.crng', ['cython_example/random/crng.pyx'],
-        include_dirs=[np.get_include()]),
-    ]
+    Extension(
+        "cython_example.random.crng",
+        ["cython_example/random/crng.pyx"],
+        include_dirs=[np.get_include()],
+    ),
+]
 
 # See https://cython.readthedocs.io/en/latest/src/userguide/source_files_and_compilation.html
 # for a deeper explanation of the choices here
 Cython.Compiler.Options.docstring = False
 Cython.Compiler.Options.error_on_uninitialized = True
 directives = {
-    'language_level': '3', # We assume Python 3 code
-    'boundscheck': False, # Do not check array access
-    'wraparound': False, # a[-1] does not work
-    'embedsignature': False, # Do not save typing / docstring
-    'always_allow_keywords': False, # Faster calling conventions
-    'initializedcheck': False, # We assume memory views are initialized
+    "language_level": "3",  # We assume Python 3 code
+    "boundscheck": False,  # Do not check array access
+    "wraparound": False,  # a[-1] does not work
+    "embedsignature": False,  # Do not save typing / docstring
+    "always_allow_keywords": False,  # Faster calling conventions
+    "initializedcheck": False,  # We assume memory views are initialized
 }
 
 setup(
-    name='cython_example',
-    version='0.0.1',
-    packages=['cython_example'],
-    author='Juan Jose Garcia-Ripoll',
-    author_email='juanjose.garciaripoll@gmail.com',
+    name="cython_example",
+    version="0.0.1",
+    packages=["cython_example"],
+    author="Juan Jose Garcia-Ripoll",
+    author_email="juanjose.garciaripoll@gmail.com",
     description="An example for a Cython-based package.",
-    url='https://juanjose.garciaripoll.com',
-    ext_modules=cythonize(extensions, 
-                          compiler_directives=directives),
+    url="https://juanjose.garciaripoll.com",
+    ext_modules=cythonize(extensions, compiler_directives=directives),
     include_dirs=np.get_include(),
     install_requires=[
-        'numpy',
+        "numpy",
     ],
 )
--- test/test.py
+++ test/test.py
@@ -2,34 +2,37 @@ import cython_example as ce
 import numpy as np
 from timeit import Timer
 
-def timeit(stmt, setup=''):
+
+def timeit(stmt, setup=""):
     t = Timer(stmt, setup, globals=globals())
-    n = t.autorange() # calibrate No. of iterations
-    t.timeit(n[0]) # warm up
-    return t.timeit(n[0]*2)/n[0]/2/1e-6
+    n = t.autorange()  # calibrate No. of iterations
+    t.timeit(n[0])  # warm up
+    return t.timeit(n[0] * 2) / n[0] / 2 / 1e-6
+
 
 def norminfc(A):
     return np.max(np.sum(np.abs(A), -1))
 
+
 A = np.random.randn(100, 100)
 
-t = timeit('ce.matrix.norminf(A)')
-print(f'norminf: {t:1.3g} mus/iteration (Cython version a)')
+t = timeit("ce.matrix.norminf(A)")
+print(f"norminf: {t:1.3g} mus/iteration (Cython version a)")
 
-t = timeit('norminfc(A)')
-print(f'norminf: {t:1.3g} mus/iteration (Numpy version)')
+t = timeit("norminfc(A)")
+print(f"norminf: {t:1.3g} mus/iteration (Numpy version)")
 
-t = timeit('np.linalg.norm(A, ord=np.inf)')
-print(f'norminf: {t:1.3g} mus/iteration (Numpy library version)')
+t = timeit("np.linalg.norm(A, ord=np.inf)")
+print(f"norminf: {t:1.3g} mus/iteration (Numpy library version)")
 
-t = timeit('ce.matrix.trace(A)')
-print(f'trace: {t:1.3g} mus/iteration (Cython version)')
+t = timeit("ce.matrix.trace(A)")
+print(f"trace: {t:1.3g} mus/iteration (Cython version)")
 
-t = timeit('np.trace(A)')
-print(f'trace: {t:1.3g} mus/iteration (Numpy version)')
+t = timeit("np.trace(A)")
+print(f"trace: {t:1.3g} mus/iteration (Numpy version)")
 
-t = timeit('ce.matrix.random((1000,1000))')
-print(f'random: {t:1.3g} mus/iteration (Cython version)')
+t = timeit("ce.matrix.random((1000,1000))")
+print(f"random: {t:1.3g} mus/iteration (Cython version)")
 
-t = timeit('rng.random((1000,1000))', 'rng=np.random.default_rng()')
-print(f'random: {t:1.3g} mus/iteration (Numpy version)')
+t = timeit("rng.random((1000,1000))", "rng=np.random.default_rng()")
+print(f"random: {t:1.3g} mus/iteration (Numpy version)")
